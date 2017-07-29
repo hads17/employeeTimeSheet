984fe82bb64687062824722705667528d84cb28d
bradley sevison <bradleysev@gmail.com>
2017-07-29 11:02:46 -0600

    minor changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,17 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBjzDREpWoJdTOOu8Po-VThJunl4tWPwhc",
+    authDomain: "employeetimesheet-cd040.firebaseapp.com",
+    databaseURL: "https://employeetimesheet-cd040.firebaseio.com",
+    projectId: "employeetimesheet-cd040",
+    storageBucket: "employeetimesheet-cd040.appspot.com",
+    messagingSenderId: "179793583890"
+  };
+  firebase.initializeApp(config);
+
+
 $(document).ready(function(){
-    $('#submit').on('click', function(){
+    $('body').on('click', '#submit', function(){
         var name = $('#name').val().trim();
         var role = $('#role').val().trim();
         var startDate = $('#startDate').val().trim();
