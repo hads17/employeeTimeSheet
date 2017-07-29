c3b62c2e6f0f4d78cb0e7a9db492f882da2d0439
bradley sevison <bradleysev@gmail.com>
2017-07-29 10:52:51 -0600

    minor changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,13 +1,14 @@
-!(document).ready(function(){
+$(document).ready(function(){
     $('#submit').on('click', function(){
-        var name = $('name').val().trim()
-        var role = $('role').val().trim()
-        var startDate = $('startDate').val().trim()
-        var monthlyRate = $('monthlyRate').val().trim()
-        var monthsWorked = ;
-        var totalBilled = ;
+        var name = $('#name').val().trim();
+        var role = $('#role').val().trim();
+        var startDate = $('#startDate').val().trim();
+        var monthlyRate = $('#monthlyRate').val().trim();
+        var monthsWorked;
+        var totalBilled;
+        console.log('hello');
 
-        $('#table1').append('')
+        $('#table1').append('<tr>' + '<td>'+ name + '</td>' + '</tr');
 
     });
 });
